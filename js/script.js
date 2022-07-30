a9fd0cbfb2e2468959c7eb92fabdd3ec56ab3ccc
layalWanni <nissolelo03@gmail.com>
2022-07-30 12:39:30 -0300

    header update

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#dadee3';
+    //header.style.backgroundColor = '#29323c';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
